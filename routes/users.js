d38e242b1658cdb79f2efdde0802d9810244ca88
DIVYANSH MASIWAL <60735977+BAAHUBALI7781@users.noreply.github.com>
2021-04-09 15:08:01 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,3 @@
-// const express=require('express');
-// const router=express();
-
-// const userController=require('../controllers/user_controller');
-// router.get('/profile',userController.profile);
-// module.exports=router;
-
-
-
 const express=require('express');
 const { use } = require('.');
 const router=express.Router();
