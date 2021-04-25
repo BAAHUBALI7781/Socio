138af6734fda5f999527896b9f612e526cae9f6e
baahubali7781 <divyanshm1609@gmail.com>
2021-04-25 11:13:03 +0530

    Minor change in passport-local

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(passport.setAunthenticatedUser);
+app.use(passport.setAuthenticatedUser);
 
 //use express router
 app.use('/',require('./routes/index'));
