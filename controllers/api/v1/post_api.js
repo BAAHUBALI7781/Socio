aed33aa08d1b3a3a930e7e2cde60bfb3458ecb6e
baahubali7781 <divyanshm1609@gmail.com>
2021-05-04 17:57:56 +0530

    Deleting posts through APIs(without authentication)

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,6 +1,37 @@
-module.exports.index=function(req,res){
-    return res.json(200,{
+const Post=require('../../../models/post');
+const Comment=require('../../../models/comments');
+module.exports.index=async function(req,res){
+
+    let posts=await Post.find({})
+        .sort('-createdAt')
+        .populate('user','user_name')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+        console.log(posts);
+        return res.json(200,{
         message:'List of posts',
-        posts:[]
+        posts:posts
     })
+}
+
+module.exports.destroy=async function(req,res){
+
+    try{
+        let post=await Post.findById(req.params.id);
+        
+        post.remove();
+        await Comment.deleteMany({post:req.params.id});
+        return res.json(200,{
+            message:'Post and associated comments deleted'
+        })
+    }catch(err){
+        return res.json(500,{
+            message:'Internal Server Error'
+        });
+    }
+
 }
--- routes/api/v1/posts_v1.js
+++ routes/api/v1/posts_v1.js
@@ -3,5 +3,5 @@ const router=express.Router();
 
 const postApi=require('../../../controllers/api/v1/post_api');
 router.get('/',postApi.index);
-
+router.delete('/:id',postApi.destroy);
 module.exports=router;
