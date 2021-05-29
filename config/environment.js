0e6e744337da36260e969081dac112fbac8f3841
baahubali7781 <divyanshm1609@gmail.com>
2021-05-29 18:43:46 +0530

    Add logger using morgan and rfs

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,15 @@
+const fs=require('fs');
+const rfs=require('rotating-file-stream');
+const path=require('path');
+const log_directory=path.join(__dirname,'../production_logs');
+
+fs.existsSync(log_directory) || fs.mkdirSync(log_directory);
+const accessLogStream=rfs.createStream('access.log',{
+    interval:'1d',
+    path:log_directory
+});
+
+
 const development={
     name:'development',
     asset_path:'./assets',
@@ -16,7 +28,11 @@ const development={
     google_clientID:"45497913421-qgvtooat8lsblc9dpahl11crgnij1s8h.apps.googleusercontent.com",
     google_clientSecret:"97iz_nBJArWBnxvfSRRdY8tl",
     google_callbackURL:"http://localhost:8080/user/auth/google/callback",
-    jwt_secret:'Socio'
+    jwt_secret:'Socio',
+    morgan:{
+        mode:'dev',
+        options:{stream:accessLogStream}
+    }
 }
 const production={
     name:'production',
@@ -36,7 +52,11 @@ const production={
     google_clientID: process.env.SOCIO_GOOGLE_CLIENTID,
     google_clientSecret: process.env.SOCIO_GOOGLE_CLIENTSECRET,
     google_callbackURL: process.env.SOCIO_GOOGLE_CALLBACK,
-    jwt_secret: process.env.SOCIO_JWT_SECRET
+    jwt_secret: process.env.SOCIO_JWT_SECRET,
+    morgan:{
+        mode:'combined',
+        options:{stream:accessLogStream}
+    }
 }
 
 module.exports=eval(process.env.SOCIO_ENVIRONMENT) == undefined ? development:eval(process.env.SOCIO_ENVIRONMENT);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 const env=require('./config/environment');
+const logger=require('morgan');
 const port=8080;
 const app=express();
 // To create layouts
@@ -45,6 +46,7 @@ app.use(express.urlencoded());
 
 app.use(cookieParser());
 
+app.use(logger(env.morgan.mode,env.morgan.options));
 // Use the assets 
 app.use(express.static(env.asset_path));
 // Make the uploads path available to the browser
