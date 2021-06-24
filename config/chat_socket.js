8063b2ca2701a42c289572645d78864ddf21d797
Divyansh <divyanshm1609@gmail.com>
2021-06-24 21:44:44 +0530

    Chat socket async await

--- config/chat_socket.js
+++ config/chat_socket.js
@@ -1,32 +1,5 @@
 const Chat = require('../models/chat');
-module.exports.chatSocket = function(chatServer){
-    let io = require('socket.io')(chatServer);
 
-    io.sockets.on('connection', function(socket){
-        console.log('new connection received', socket.id);
-        socket.on('disconnect', function(){
-            console.log("Socket disconnected");
-        });
-        socket.on('join_room',async function(data){
-            var date=new Date();
-            const obj=convert(date);
-            data.date=obj.rdate;
-            data.time=onj.rtime;
-            const newMessage=await Chat.create(data);
-            socket.join(data.room_id);
-            io.in(data.room_id).emit('user_join',data);
-        });
-        socket.on('send',async function(data){
-            var date=new Date();
-            const obj=convert(date);
-            data.date=obj.rdate;
-            data.time=onj.rtime;
-            const newMessage=await Chat.create(data);
-            io.in(data.room_id).emit('receive',data);
-        })
-
-    });
-}
 function convert(date){
     date.setHours(date.getHours() + 5);
     date.setMinutes(date.getMinutes() + 30);
@@ -42,3 +15,33 @@ function convert(date){
         rtime:rtime
     }
 }
+module.exports.chatSocket = function(chatServer){
+    let io = require('socket.io')(chatServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+        socket.on('disconnect', function(){
+            console.log("Socket disconnected");
+        });
+        socket.on('join_room',async function(data){
+            var date=new Date();
+            const obj=await convert(date);
+            data.date=obj.rdate;
+            data.time=obj.rtime;
+            const newMessage=await Chat.create(data);
+            console.log(newMessage);
+            socket.join(data.room_id);
+            io.in(data.room_id).emit('user_join',data);
+        });
+        socket.on('send',async function(data){
+            var date=new Date();
+            const obj=convert(date);
+            data.date=obj.rdate;
+            data.time=obj.rtime;
+            const newMessage=await Chat.create(data);
+            io.in(data.room_id).emit('receive',data);
+        })
+
+    });
+}
+
