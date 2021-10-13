e805c5feda4419b1aa63c4f47428dafb3b50697b
Divyansh <divyanshm1609@gmail.com>
2021-10-13 17:12:06 +0530

    Chat settings changes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const customFlash=require('./config/middleware');
 // Socket.io
 const chatServer=require('http').Server(app);
 const chatSockets=require('./config/chat_socket').chatSocket(chatServer);
-chatServer.listen(5000);
+chatServer.listen(port);
 console.log("Chat server is listening on port 5000");
 
 // Setting up SCSS
