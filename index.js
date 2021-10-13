823c902af15f7343dfaf6035d0ce6b310dfee3a5
Divyansh <divyanshm1609@gmail.com>
2021-10-13 16:39:40 +0530

    Added Port for deploymenty

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 const logger=require('morgan');
-const port=8080;
+const port=process.env.PORT || 8080;
 const app=express();
 
 const path=require('path');
